41d4bd654edc9bc2746d82087ed1de72e3e27ce4
Deepanshu Singh <deepanshu17092003@gmail.com>
2022-11-05 11:08:48 +0530

    Changed routes

--- navbar.php
+++ navbar.php
@@ -1,8 +1,8 @@
 <nav class="text-gray-600 bg-[#4e53c8] body-font">
     <div class="flex flex-wrap justify-between p-3 flex-col md:flex-row items-center">
-        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="./index.php">
+        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="/dbms-project/index.php">
             <span class="ml-3 text-xl text-white">Students Discussion Forum</span>
         </a>
-        <a href="./logout.php"><button class="inline-flex md:ml-auto items-center bg-white border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Logout</button></a>
+        <a href="/dbms-project/logout.php"><button class="inline-flex md:ml-auto items-center bg-white border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Logout</button></a>
     </div>
 </nav>
